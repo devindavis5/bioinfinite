20819bfc2ff8282c310baf660517bbd6ea3e4391
devindavis5 <devinrdavis555@gmail.com>
2020-11-07 13:50:45 -0600

    added buttons to homepage

--- front-end/src/components/SignIn.js
+++ front-end/src/components/SignIn.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import MainContainer from '../containers/MainContainer.js'
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 
 export default class SignIn extends Component {
     render() {
         return (
             <div>
-                <h1>SignIn Component</h1>
+                {/* <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form> */}
                 <MainContainer />
+                {/* <NavLink to="/maincontainer" exact>Movies </NavLink> */}
             </div>
         )
     }
