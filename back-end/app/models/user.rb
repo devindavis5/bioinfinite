251baaf25b49f3c38ceefd06ef38c512a0d41187
devindavis5 <devinrdavis555@gmail.com>
2020-11-09 16:04:17 -0600

    login almost done

--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :quizzes
     has_many :categories, through: :quizzes
+    has_secure_password
+    validates :name, uniqueness: { case_sensitive: false }
+
 end
