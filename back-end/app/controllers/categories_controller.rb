4bd4acd6a5cd8166b154d413c4bd4a17a1aa489a
breewarren <ambreea.warren@gmail.com>
2020-11-10 15:43:24 -0600

    category iteration to show page working

--- back-end/app/controllers/categories_controller.rb
+++ back-end/app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+    skip_before_action :authorized, only: [:index]
 
     def index
         categories = Category.all
--- front-end/src/containers/MainContainer.js
+++ front-end/src/containers/MainContainer.js
@@ -5,14 +5,28 @@ import Footer from '../components/Footer.js'
 import UserShowPanel from '../components/UserShowPanel.js'
 
 export default class MainContainer extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            categories: []
+        }
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3000/categories").then(res => res.json())
+        .then(categories => this.setState({categories}))
+    }
+    
     render() {
         return (
             <div>
                 <NavBar />
                 <h1>MainContainer Component</h1>
-                <Carousel />
+                <Carousel categories={this.state.categories}/>
                 <Footer />
                 <UserShowPanel/>
+                
             </div>
         )
     }
