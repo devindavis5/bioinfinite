bc7e1280dbafa4cf0828b4b43d594c1edd299097
devindavis5 <devinrdavis555@gmail.com>
2020-11-10 19:44:20 -0600

    backend show pages

--- back-end/app/controllers/sub_categories_controller.rb
+++ back-end/app/controllers/sub_categories_controller.rb
@@ -1,7 +1,13 @@
 class SubCategoriesController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
 
     def index
         sub_categories = SubCategory.all
+        render json: sub_categories, include: [:category]
+    end
+
+    def show
+        sub_categories = SubCategory.where(category_id: params[:id])
         render json: sub_categories
     end
 
--- back-end/app/models/category.rb
+++ back-end/app/models/category.rb
@@ -1,6 +1,11 @@
+
 class Category < ApplicationRecord
     has_many :quizzes
     has_many :users, through: :quizzes
     has_many :questions
     has_many :subcategories
+
+    # def subcategories(id)
+    #     SubCategory.all.select {|s| s.category_id == id}
+    # end
 end
